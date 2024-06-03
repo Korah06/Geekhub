eeae65197395eb351dfe78db0b3088a62440bfa4
Mario <mario.espasa.2003@gmail.com>
2024-06-03 19:28:25 +0200

    Basic LoginScreen

--- app/src/main/java/com/geekgub/geekhub/ui/screens/LoginScreen.kt
+++ app/src/main/java/com/geekgub/geekhub/ui/screens/LoginScreen.kt
@@ -4,11 +4,19 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -17,10 +25,12 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import com.geekgub.geekhub.ui.theme.CardBackground
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun LoginScreen() {
 
@@ -33,22 +43,61 @@ fun LoginScreen() {
         verticalArrangement = Arrangement.Center
     ) {
         var text by remember { mutableStateOf("") }
+        var password by remember { mutableStateOf("") }
+//        var passwordVisible by remember { mutableStateOf(false) }
 
         Box(
             contentAlignment = Alignment.Center,
             modifier = Modifier
-                .padding(16.dp)
+                .padding(32.dp)
                 .clip(RoundedCornerShape(10.dp))
                 .background(color = CardBackground)
         ) {
-            Column {
+            Column(modifier = Modifier.padding(10.dp)) {
                 Text(text = "GeekHub")
                 TextField(
                     value = text,
                     onValueChange = { newText -> text = newText },
-                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email)
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
+                    label = { Text("Email") },
+                    singleLine = true,
+                    shape = RoundedCornerShape(10.dp),
+                    leadingIcon = {
+                        Icon(imageVector = Icons.Default.Email, contentDescription = "Email" )
+                    },
+                    colors = TextFieldDefaults.colors(
+                        unfocusedIndicatorColor = Color.Transparent,
+                        focusedIndicatorColor = Color.Transparent
+                    ),
+                    modifier = Modifier
+                        .padding(16.dp)
+                        .fillMaxWidth()
                 )
-
+                TextField(
+                    value = password,
+                    onValueChange = { newPassword -> password = newPassword },
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
+                    label = { Text("Password") },
+                    singleLine = true,
+                    shape = RoundedCornerShape(10.dp),
+                    leadingIcon = {
+                        Icon(imageVector = Icons.Default.Lock, contentDescription = "Password" )
+                    },
+                    colors = TextFieldDefaults.colors(
+                        unfocusedIndicatorColor = Color.Transparent,
+                        focusedIndicatorColor = Color.Transparent
+                    ),
+                    modifier = Modifier
+                        .padding(16.dp)
+                        .fillMaxWidth(),
+                )
+                Button(
+                    onClick = { /*FUNCTIONALITY*/ },
+                    modifier = Modifier.fillMaxWidth(),
+                    shape = RoundedCornerShape(10.dp)
+                ) {
+                    Text("Login")
+                }
             }
         }
     }
